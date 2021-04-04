1b181af8f7cd93564a30f9dd30955cd99d601dcd
Vamsi Kalapala <vamsikalapala@Vamsis-MacBook-Pro.local>
2021-04-03 20:10:24 -0700

    adding some label support

--- npm-chaos.sh
+++ npm-chaos.sh
@@ -2,6 +2,32 @@
 
 #alias k=kubectl
 namespaces=(web0 web01 web02 web03 web04 web05 web06 web07 web08 web09 web10)
+labelsArray=(chaos=true)
+
+generateNs () {
+    for i in {1..100}
+    do
+        #sufix=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
+        sufix=test
+        namespaces=("${namespaces[@]}" "web-$sufix-$i")
+    done
+}
+
+generateLabels () {
+    labelsArray=(chaos=true)
+    for i in {1..100}
+    do
+        labelKey=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
+        labelVal=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
+        labelsArray=("${labelsArray[@]}" "chaos-$labelKey=$labelVal")
+    done
+}
+
+echo "Generating $numOfNs namespaces"
+generateNs
+echo "Done Generating NS"
+
+echo ${namespaces[@]}
 
 kubectl create ns test1replace
 for i in ${namespaces[@]}; do
@@ -17,9 +43,8 @@ for ns in ${namespaces[@]}; do
     echo "Applying Labels to $ns"
     for i in {1..30}
     do               
-        labelKey=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
-        labelVal=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
-        kubectl label pods -n $ns --all chaos-$labelKey=$labelVal
+        generateLabels
+        kubectl label pods -n $ns --all ${labelsArray[@]}
     done
 done
 
@@ -35,16 +60,19 @@ for i in $(seq 1 50);do
     for ns in ${namespaces[@]}; do
         echo "Deleting random pods in namespace $ns"
         #list all pods in the namespace into a file
-        kubectl get pods -n $ns | awk '{print $1}' > testkgp
+        kubectl get pods -n $ns | awk '{print $1}' > pods_in_ns.txt
 
         #get 10 random pods and delete them
         podname=$(shuf -n 10 testkgp | xargs)
         kubectl delete pod -n $ns $podname
+        sleep 3
+
+        generateLabels
+        kubectl label pods -n $ns --all ${labelsArray[@]}
+
     done
     
     sleep 5
 done
 
-#for i in ${namespaces[@]}; do
-#    k delete ns $i
-#done
+#k delete ns ${namespaces[@]}
